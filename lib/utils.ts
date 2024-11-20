2bf4e7e5811910faa51501a72cac6df75fe0da49
Anirudh Kamath <github@kamath.io>
2024-11-19 19:46:46 -0800

    log errors better

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export function generateId(operation: string) {
 export function logLineToString(logLine: LogLine): string {
   const timestamp = logLine.timestamp || new Date().toISOString();
   if (logLine.auxiliary?.error) {
-    return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message}\n ${JSON.stringify(logLine.auxiliary.error)}\n ${JSON.stringify(logLine.auxiliary.trace)}`;
+    return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message}\n ${logLine.auxiliary.error.value}\n ${logLine.auxiliary.trace.value}`;
   }
   return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message} ${
     logLine.auxiliary ? JSON.stringify(logLine.auxiliary) : ""
