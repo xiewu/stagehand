9700e6db2a39d828e8bbcb5b27a71a7dc1f31f6b
Navid Pour <navid.pour@outlook.com>
2024-11-23 23:56:55 -0800

    error handling for log method

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,13 +7,18 @@ export function generateId(operation: string) {
 }
 
 export function logLineToString(logLine: LogLine): string {
+  try {
   const timestamp = logLine.timestamp || new Date().toISOString();
-  if (logLine.auxiliary?.error) {
-    return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message}\n ${logLine.auxiliary.error.value}\n ${logLine.auxiliary.trace.value}`;
+    if (logLine.auxiliary?.error) {
+      return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message}\n ${logLine.auxiliary.error.value}\n ${logLine.auxiliary.trace.value}`;
+    }
+    return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message} ${
+      logLine.auxiliary ? JSON.stringify(logLine.auxiliary) : ""
+    }`;
+  } catch (error) {
+    console.error(`Error logging line:`, logLine);
+    return "error logging line";
   }
-  return `${timestamp}::[stagehand:${logLine.category}] ${logLine.message} ${
-    logLine.auxiliary ? JSON.stringify(logLine.auxiliary) : ""
-  }`;
 }
 
 export function validateZodSchema(schema: z.ZodTypeAny, data: any) {
