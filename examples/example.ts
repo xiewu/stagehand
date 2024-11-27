7645054b22f331f26b9033a218acaca1516316db
Paul Klein <paulleoklein@gmail.com>
2024-11-26 22:13:33 -0800

    Don't change example

--- examples/example.ts
+++ examples/example.ts
@@ -3,50 +3,23 @@ import { z } from "zod";
 
 async function example() {
   const stagehand = new Stagehand({
-    env: "BROWSERBASE",
-    debugDom: false,
+    env: "LOCAL",
+    verbose: 1,
+    debugDom: true,
+    enableCaching: false,
   });
 
-  await stagehand.init({ modelName: "claude-3-5-sonnet-20241022" });
-
-  // Navigate to Hacker News
-  await stagehand.page.goto("https://www.zolve.com");
-  await stagehand.act({
-    action: "Close any visible popups or banners",
-    useVision: false,
+  await stagehand.init();
+  await stagehand.page.goto("https://github.com/browserbase/stagehand");
+  await stagehand.act({ action: "click on the contributors" });
+  const contributor = await stagehand.extract({
+    instruction: "extract the top contributor",
+    schema: z.object({
+      username: z.string(),
+      url: z.string(),
+    }),
   });
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const viewportHeight = await stagehand.page.evaluate(
-    () => window.innerHeight,
-  );
-  let currentScrollPosition = 0;
-  const totalHeight = await stagehand.page.evaluate(
-    () => document.body.scrollHeight,
-  );
-
-  while (currentScrollPosition < totalHeight) {
-    await stagehand.page.evaluate((scrollBy) => {
-      window.scrollBy(0, scrollBy);
-    }, viewportHeight);
-
-    currentScrollPosition += viewportHeight;
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second between each scroll
-  }
-  await stagehand.page.evaluate(() => {
-    window.scrollTo(0, 0);
-  });
-  await stagehand.page.setViewportSize({
-    width: 1920,
-    height: await stagehand.page.evaluate(() => document.body.scrollHeight),
-  });
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  await stagehand.page.screenshot({ fullPage: true, path: "screenshot.png" });
-
-  await new Promise((resolve) => setTimeout(resolve, 100000));
-
-  await stagehand.context.close();
+  console.log(`Our favorite contributor is ${contributor.username}`);
 }
 
 (async () => {
