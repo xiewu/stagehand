7e9a83c165fbc7ce8ac3b058d6b78f28a8a47553
Paul Klein <paulleoklein@gmail.com>
2024-11-26 21:46:22 -0800

    Add side-by-side example for playwright vs stagehand

--- examples/example.ts
+++ examples/example.ts
@@ -3,23 +3,50 @@ import { z } from "zod";
 
 async function example() {
   const stagehand = new Stagehand({
-    env: "LOCAL",
-    verbose: 1,
-    debugDom: true,
-    enableCaching: false,
+    env: "BROWSERBASE",
+    debugDom: false,
   });
 
-  await stagehand.init();
-  await stagehand.page.goto("https://github.com/browserbase/stagehand");
-  await stagehand.act({ action: "click on the contributors" });
-  const contributor = await stagehand.extract({
-    instruction: "extract the top contributor",
-    schema: z.object({
-      username: z.string(),
-      url: z.string(),
-    }),
+  await stagehand.init({ modelName: "claude-3-5-sonnet-20241022" });
+
+  // Navigate to Hacker News
+  await stagehand.page.goto("https://www.zolve.com");
+  await stagehand.act({
+    action: "Close any visible popups or banners",
+    useVision: false,
   });
-  console.log(`Our favorite contributor is ${contributor.username}`);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const viewportHeight = await stagehand.page.evaluate(
+    () => window.innerHeight,
+  );
+  let currentScrollPosition = 0;
+  const totalHeight = await stagehand.page.evaluate(
+    () => document.body.scrollHeight,
+  );
+
+  while (currentScrollPosition < totalHeight) {
+    await stagehand.page.evaluate((scrollBy) => {
+      window.scrollBy(0, scrollBy);
+    }, viewportHeight);
+
+    currentScrollPosition += viewportHeight;
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second between each scroll
+  }
+  await stagehand.page.evaluate(() => {
+    window.scrollTo(0, 0);
+  });
+  await stagehand.page.setViewportSize({
+    width: 1920,
+    height: await stagehand.page.evaluate(() => document.body.scrollHeight),
+  });
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  await stagehand.page.screenshot({ fullPage: true, path: "screenshot.png" });
+
+  await new Promise((resolve) => setTimeout(resolve, 100000));
+
+  await stagehand.context.close();
 }
 
 (async () => {
