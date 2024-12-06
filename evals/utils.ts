a4050920630324a987a7b5db051ddf6c8fe3c23f
Sameel <sameel.m.arif@gmail.com>
2024-12-06 13:33:29 -0500

    Feat: Add ESLint (#274)
    * add eslint (#269)
    
    * add eslint
    
    * update vision typing
    
    * type fixes
    
    * update anthropic types
    
    * update openai types
    
    * fix type errs
    
    * type fixes
    
    * revert args map
    
    * createChatCompletion typing
    
    * eslint ignore dist
    
    * type fixes
    
    * cache/eval type fixes
    
    * update expected type
    
    * add nonsense_action
    
    * changeset
    
    * ignore dom build
    
    * Add eslint edits (#273)
    
    * add eslint
    
    * update vision typing
    
    * type fixes
    
    * update anthropic types
    
    * update openai types
    
    * fix type errs
    
    * type fixes
    
    * revert args map
    
    * createChatCompletion typing
    
    * eslint ignore dist
    
    * type fixes
    
    * cache/eval type fixes
    
    * update expected type
    
    * add nonsense_action
    
    * changeset
    
    * ignore dom build
    
    * remove casting
    
    * add auxillary
    
    * more cleanup
    
    * type cleaning
    
    * type cleaning
    
    * update changeset
    
    * fix last 3 type errs
    
    * expedia error handling
    
    * evals type safety

--- evals/utils.ts
+++ evals/utils.ts
@@ -96,6 +96,6 @@ export function normalizeString(str: string): string {
   return str
     .toLowerCase()
     .replace(/\s+/g, " ")
-    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, "")
     .trim();
 }
