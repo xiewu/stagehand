2a1ce6c29a65d700e795849b46744b954e913cb5
Miguel <miguelg71921@gmail.com>
2025-01-17 12:35:48 -0800

    PR cleanup

--- lib/StagehandPage.ts
+++ lib/StagehandPage.ts
@@ -405,7 +405,6 @@ export class StagehandPage {
         requestId,
         domSettleTimeoutMs,
         useTextExtract,
-        useAccessibilityTree,
       })
       .catch((e) => {
         this.stagehand.log({
