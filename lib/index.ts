deef994434c9804e0d6396393eba262ac4dade28
Sean McGuire <seanmcguire1@outlook.com>
2025-03-04 18:57:19 -0800

    idt navid is gonna get to this todo

--- lib/index.ts
+++ lib/index.ts
@@ -687,24 +687,6 @@ export class Stagehand {
             );
         })
         .catch(() => {
-          // NAVIDTODO: Rerun the log call on the new page
-          // This is expected to happen when the user is changing pages
-          // console.error("Logging Error:", e);
-          // this.log({
-          //   category: "browserbase",
-          //   message: "error logging to browserbase",
-          //   level: 1,
-          //   auxiliary: {
-          //     trace: {
-          //       value: e.stack,
-          //       type: "string",
-          //     },
-          //     message: {
-          //       value: e.message,
-          //       type: "string",
-          //     },
-          //   },
-          // });
         });
     }
   }
