6f4f855bbdf17a94cf3ef7dc3e5fc6c7aa48f3a4
Anirudh Kamath <github@kamath.io>
2025-03-06 11:14:56 -0800

    comment

--- examples/example.ts
+++ examples/example.ts
@@ -17,6 +17,7 @@ async function example() {
   /**
    * Add your code here!
    */
+  // delete before pushing to main
   const tree = await stagehand.page.getAccessibilityTree();
   console.log(tree);
   await stagehand.close();
