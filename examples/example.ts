693fc6f6768016a610703a234779b6917e2736a3
Anirudh Kamath <github@kamath.io>
2025-03-30 23:31:04 -0700

    gif + cleanup

--- examples/example.ts
+++ examples/example.ts
@@ -7,16 +7,41 @@
 
 import { Stagehand } from "@/dist";
 import StagehandConfig from "@/stagehand.config";
+import { z } from "zod";
 
 async function example() {
   const stagehand = new Stagehand({
     ...StagehandConfig,
   });
   await stagehand.init();
-  await stagehand.page.goto("https://docs.stagehand.dev");
-  /**
-   * Add your code here!
-   */
+  const page = stagehand.page;
+  await page.goto("https://github.com/browserbase");
+
+  // Use Stagehand's act() to control the page
+  const [result] = await page.observe({
+    instruction: "click on the stagehand repo",
+    drawOverlay: true,
+  });
+  await page.act(result);
+
+  // Use Computer Use agents with one line of code
+  // These are useful for actions that are a series of steps
+  const agent = stagehand.agent({
+    provider: "openai",
+    model: "computer-use-preview",
+  });
+  await agent.execute("Get to the latest PR");
+
+  // Use Stagehand's extract() to extract data from the page
+  const { author, title } = await page.extract({
+    instruction: "extract the author and title of the PR",
+    schema: z.object({
+      author: z.string().describe("The username of the PR author"),
+      title: z.string().describe("The title of the PR"),
+    }),
+  });
+  console.log(`Author: ${author}`);
+  console.log(`Title: ${title}`);
   await stagehand.close();
 }
 
