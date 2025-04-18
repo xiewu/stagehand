469be4412eb563f0a31b22fed5b0b805398d0549
Sean McGuire <seanmcguire1@outlook.com>
2025-04-17 21:44:50 -0700

    rm unused imports

--- lib/handlers/handlerUtils/actHandlerUtils.ts
+++ lib/handlers/handlerUtils/actHandlerUtils.ts
@@ -1,4 +1,4 @@
-import { Page, Locator, errors as PlaywrightErrors } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 import { PlaywrightCommandException } from "../../../types/playwright";
 import { StagehandPage } from "../../StagehandPage";
 import { getNodeFromXpath } from "@/lib/dom/utils";
